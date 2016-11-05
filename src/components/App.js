0f88e3f98f483ea236e54ddf4ad84814645cbaf7
Egemen Can Uze <egemencanuze@gmail.com>
2016-11-05 12:19:39 +0200

    Add wrong guesses

--- src/components/App.js
+++ src/components/App.js
@@ -35,7 +35,7 @@ class App extends Component {
   handleGuess(input) {
     let NewGuesses = this.state.guesses;
     input.split("").forEach((guess) => {
-      if(NewGuesses.indexOf(guess) === -1) {
+      if(guess !== " " && NewGuesses.indexOf(guess) === -1) {
         NewGuesses.push(guess);
       }
     });
@@ -43,12 +43,20 @@ class App extends Component {
   }
 
   render() {
+    let word = this.state.word;
+    let guesses = this.state.guesses;
+    let wrongGuesses = guesses.map((letter) => {
+      if(word.indexOf(letter) === -1) {
+        return letter;
+      }
+    });
     return (
       <div>
         <h1 className="text-center">Hangman Game</h1>
         <WordToGuess word={this.state.word} guesses={this.state.guesses}/>
         <GuessForm handleGuess={this.handleGuess.bind(this)}/>
         <NewGameButton newGame={this.newGame.bind(this)}/>
+        <p>Wrong Guesses: {wrongGuesses}</p>
       </div>
     );
   }
